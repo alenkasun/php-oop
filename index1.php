c71b0d02a056c8cf301d074a367c196815485bc9
Alyona Shevchenko <alenka_programmer@mail.ru>
2016-04-05 15:55:20 +0300

    compare procedural and oop approaches

---
+++ index1.php
@@ -0,0 +1,39 @@
+<?php
+// set a database connection
+   $dbname = "zfdemo";
+   $username = "root";
+   $password = "root";
+    try {
+        $conn = new PDO('mysql:host=localhost;dbname='.$dbname . ';charset=utf8', $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+    catch (Exception $e) {
+        die($e->getMessage());
+    }
+
+// run a query
+    $sql = "SELECT * FROM `movies` LIMIT 0, 3";
+    try {
+        $result = $conn->query($sql);
+    }
+    catch (Excaption $e) {
+        die($e->getMessage());
+    }
+// display post titles
+    if($result->rowCount()){
+        foreach ($result as $post) {
+            echo '<h2>' . $post['title'] . '</h2>';
+        }
+    }
+
+
+// OOP style
+/*
+    $post = new PostModel();
+    $posts = $post->fetchAll();
+    foreach ($posts as $post) {
+        echo '<h1>' . $post->title . '</h1>';
+    }
+*/
+
+?>
