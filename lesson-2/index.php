1ef1bade9cb9072b1029ae444bcefb2cf55079a3
Alyona Shevchenko <alenka_programmer@mail.ru>
2016-04-05 17:46:09 +0300

    look into difference between clone() and working by ref

--- lesson-2/index.php
+++ lesson-2/index.php
@@ -5,9 +5,20 @@
     $user1 = new User();
     $user1->setEmail("bill@gmail.com");
     $user1->setPassword("123654fg");
-    //var_dump($user1->password);   // ERROR !!! access to private property
-    var_dump($user1->getEmail());
-    echo "<br />";
-    var_dump($user1->getPassword());
 
+// save by ref: both objects changes
+   $user2 = $user1;
+   $user1->setEmail("sam@gmail.com");
+   $user2->setEmail("tom@gmail.com");
+
+// create copy of obj: both objects are completely separfted
+   $user3 = clone $user1;
+   $user1->setEmail("bill@gmail.com");
+   $user3->setEmail("din@gmail.com");
+
+    var_dump($user2);
+    echo "<br />";
+    var_dump($user2);
+    echo "<br />";
+    var_dump($user3);
 ?>
