d2160fdbba3d67e9d16ad65541e2bd22445c63ad
Alyona Shevchenko <alenka_programmer@mail.ru>
2016-04-05 17:31:01 +0300

    repeat scope in OOP using visibility markers

--- lesson-2/index.php
+++ lesson-2/index.php
@@ -1,15 +1,13 @@
 <?php
+    ini_set('display_errors','On');
     require "user.php";
 
     $user1 = new User();
-    $user1->email = "bill@gmail.com";
+    $user1->setEmail("bill@gmail.com");
     $user1->setPassword("123654fg");
-    var_dump($user1->password);
+    //var_dump($user1->password);   // ERROR !!! access to private property
+    var_dump($user1->getEmail());
+    echo "<br />";
+    var_dump($user1->getPassword());
 
-    $user2 = new User();
-    $user2->email = "sam@gmail.com";
-    $user2->password = "456987";
-    var_dump($user2);
-
-    var_dump($user1->login());
 ?>
--- lesson-2/user.php
+++ lesson-2/user.php
@@ -1,7 +1,7 @@
 <?php
     class User {
-        public $email;
-        public $password;
+        private $email;
+        private $password;
         const MINCHARS = 8;
 
         public function login(){
@@ -12,12 +12,32 @@
             return "Logging out ...";
         }
 
+        private function validatePassword($str){
+            return (strlen($str) >= self::MINCHARS)? true : false;
+        }
+
         public function setPassword($str){
-            if (strlen($str) < self::MINCHARS) {
-                die("Error: not invalid password");
-               //throw new Exception('The password shuold be at least ' . self::MINCHARS . ' characters long.');
+            if ($this->validatePassword($str) == false) {
+               throw new Exception('The password shuold be at least ' . self::MINCHARS . ' characters long.');
             }
             $this->password = hash('sha256', $str);
         }
+
+        public function getPassword(){
+            return $this->password;
+        }
+
+        public function setEmail($str){
+            if (! filter_var($str, FILTER_VALIDATE_EMAIL)) {
+               throw new Exception('Please, provide a valid email.');
+            }
+            $this->email = $str;
+        }
+
+        public function getEmail(){
+            return $this->email;
+        }
+
+
     }
 ?>
